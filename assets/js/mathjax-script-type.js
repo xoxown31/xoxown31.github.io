4f6a8feaf99a138d37f95ff227452d2b696671f9
xoxown <xoxown31@gmail.com>
2025-07-07 12:31:59 +0900

    Gemfile.lock 및 MathJax 관련 파일 업데이트: Windows 플랫폼에 대한 의존성 추가 및 MathJax 스크립트 개선

--- assets/js/mathjax-script-type.js
+++ assets/js/mathjax-script-type.js
@@ -1,7 +1,18 @@
-// This script ensures MathJax processes <script type="math/tex"> blocks
-// See: https://docs.mathjax.org/en/latest/web/typeset.html
-window.addEventListener('DOMContentLoaded', function() {
-  if (window.MathJax) {
-    MathJax.typesetPromise();
+// Copied from https://docs.mathjax.org/en/latest/upgrading/v2.html#changes-in-the-mathjax-api
+MathJax = {
+  options: {
+    renderActions: {
+      findScript: [10, function (doc) {
+        for (const node of document.querySelectorAll('script[type^="math/tex"]')) {
+          const display = !!node.type.match(/; *mode=display/);
+          const math = new doc.options.MathItem(node.textContent, doc.inputJax[0], display);
+          const text = document.createTextNode('');
+          node.parentNode.replaceChild(text, node);
+          math.start = {node: text, delim: '', n: 0};
+          math.end = {node: text, delim: '', n: 0};
+          doc.math.push(math);
+        }
+      }, '']
+    }
   }
-});
+};
